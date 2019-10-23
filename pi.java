ab8b419093de097f459314582b2d6d20f4037ae1
richard-remote <48685657+richard-remote@users.noreply.github.com>
2019-10-23 17:34:04 -0400

    Add files via upload

---
+++ pi.java
@@ -0,0 +1,44 @@
+class corvus {
+    private static String habitat = "Forest", dietGroup = "Omnivore";
+    private float length;
+    private String color;
+    
+    public String getHabitat(){
+        return habitat;
+    }
+
+    public String getDietGroup(){
+        return dietGroup;
+    }
+
+    public float getLength(){
+        return length;
+    }
+
+    public void setLength(String length){
+        this.length = lenght;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public String setColor(String color){
+        this.color = color;
+    }
+
+    /*
+    
+    atic void makingSound(){
+    sound = “I AM THE SPECIFIC CROW!”;
+    }
+    
+    void setColor(String c){
+    this.color = c;
+    }
+    
+    static void daysToEat(String animal){
+    if (animal.equals(“elephant”)
+    System.out.println(“This is going to take a while...”);
+*/
+}
---
+++ test.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class test{
+    public static void main (String[] args){
+        
+        corvus test1 = new corvus();
+        corvus test2 = new corvus();
+        
+        corvis.habitat = "Okay";
+        //test1.setPoo("NO");
+        System.out.println(test1.getHabitat());
+    }
+}
